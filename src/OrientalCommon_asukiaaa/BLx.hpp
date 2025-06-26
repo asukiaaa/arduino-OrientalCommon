6812662699d8726942069c60d789524ddb08c2d4
Asuki Kono <asukiaaa@gmail.com>
2025-06-26 17:19:47 +0900

    Remove default config value from begin of BLx

--- src/OrientalCommon_asukiaaa/BLx.hpp
+++ src/OrientalCommon_asukiaaa/BLx.hpp
@@ -6,8 +6,7 @@ namespace BLx {
 
 class Base {
  public:
-  virtual void begin(unsigned long baudrate,
-                     unsigned long config = SERIAL_8E1) = 0;
+  virtual void begin(unsigned long baudrate, unsigned long config) = 0;
   virtual void beginWithoutModbus() = 0;
   virtual uint8_t writeLock() = 0;
   virtual uint8_t writeStop() = 0;
